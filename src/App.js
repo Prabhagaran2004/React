58a5e91d23b2e974581e4626f10a42e4ecd6be24
Prabhagaran <prabhagaran158@gmail.com>
2023-11-03 15:55:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import img1 from './images/download.jpeg'
-import './App.css';
-
+import './App.css'
+import {useState} from "react"
 function App() {
+  const [name, setName]= useState("");
+  const [location, setLocation]= useState("");
+  const [fathername, setFatherName]= useState("");
+
+function onSubmit(){
+  const n=document.getElementById('name').value;
+  const l=document.getElementById('location').value;
+  const f=document.getElementById('fathername').value;
+  console.log(name);
+}
+
+
   return (
-    <div className="App">
+    <div>
+      <form>
+        <input type='text' onChange={(e)=> (setName())}
+         id="name" placeholder="Enter your name"/>
+        <input type='text' onChange={(e)=> (setLocation())}
+        id="location" placeholder="Enter your location"/>
+        <input type='text' onChange={(e)=> (setFatherName())} 
+        id="fathername" placeholder="Enter your fathername"/>
+        <button className='text' onClick={onSubmit}>Submit</button>
+      </form>
       <div>
-        hello
-      </div>
-      <div classname="app">
-        hello
-        <img src={img1} alt="image" height='100'/>
+        <p>
+          {name}
+        </p>
+        <p>
+          {location}
+        </p>
+        <p>
+          {fathername}
+        </p>
       </div>
     </div>
   );
