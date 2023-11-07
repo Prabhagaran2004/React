add5c3e4ec12cc8ece61672f2a967f6852a87c23
Prabhagaran <prabhagaran158@gmail.com>
2023-11-07 16:00:04 +0530

    wef

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import './App.css'
-import {useState} from "react"
+import { useState } from 'react';
+import './App.css';
+
 function App() {
-  const [name, setName]= useState("");
-  const [location, setLocation]= useState("");
-  const [fathername, setFatherName]= useState("");
+  const [name,setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [fathername, setFatherName] = useState('');
+  const [detail, setDetail] = useState([]);
 
-function onSubmit(){
-  const n=document.getElementById('name').value;
-  const l=document.getElementById('location').value;
-  const f=document.getElementById('fathername').value;
-  console.log(name);
-}
+  const onsubmit = (e) => {
+    console.log(name,fathername,location);
+    e.preventDefault();
+    const newDetail = {
+      name,location,fathername
+    }
+    console.log(newDetail);
+    setDetail([...detail, newDetail]);
+    console.log(detail);
+  }
 
-
+  const deleteDetail = (index) => {
+    const update = detail.filter((_,i) => i !== index);
+    setDetail(update);
+  }
+  
   return (
-    <div>
-      <form>
-        <input type='text' onChange={(e)=> (setName())}
-         id="name" placeholder="Enter your name"/>
-        <input type='text' onChange={(e)=> (setLocation())}
-        id="location" placeholder="Enter your location"/>
-        <input type='text' onChange={(e)=> (setFatherName())} 
-        id="fathername" placeholder="Enter your fathername"/>
-        <button className='text' onClick={onSubmit}>Submit</button>
+    <div className=''>
+      <form className='flex flex-col gap-4'>
+        <input type='text' onChange={(e) => setName(e.target.value)} placeholder='Enter your name' required></input>
+        <input type='text' onChange={(e) => setLocation(e.target.value)} placeholder='Enter your location' required></input>
+        <input type='text' onChange={(e) => setFatherName(e.target.value)} placeholder='Enter your father name' required></input>
+        <input type='submit' onClick={onsubmit} value={'Submit'} />
       </form>
       <div>
-        <p>
-          {name}
-        </p>
-        <p>
-          {location}
-        </p>
-        <p>
-          {fathername}
-        </p>
+        {detail.map((info, index) => (
+          <div key={index}>
+              <h1>Detail</h1>
+              <p>Name: {info.name}</p>
+              <p>Location: {info.location}</p>
+              <p>Father Name: {info.fathername}</p>
+              <button onClick={() => deleteDetail(index)}>Delete</button>
+          </div>
+        ))}
       </div>
     </div>
   );
